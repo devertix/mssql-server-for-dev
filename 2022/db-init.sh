39795e3f9ee6b696913cf087f2781635129ecaea
Bálint Dózsa <balint.dozsa@cheppers.com>
2023-11-28 12:05:51 +0100

    Fix

--- 2022/db-init.sh
+++ 2022/db-init.sh
@@ -4,7 +4,7 @@ echo "running set up script"
 
 for i in {1..100};
 do
-    /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Alma1234 -d master -i init.sql
+    /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Alma1234 -d master -i /init.sql
     if [ $? -eq 0 ]
     then
         echo "setup.sql completed"
